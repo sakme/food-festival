5635885b6e67d770317a2043b80a54a3f7f7632f
sakme <alan@akme.us>
2022-03-12 17:24:29 -0500

    final updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,36 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist"
+    filename: '[name].bundle.js',
+    path: `${__dirname}/dist`
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name (file) {
-                return "[path][name].[ext]"
+              name(file) {
+                return '[path][name].[ext]';
               },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
+              publicPath(url) {
+                return url.replace('../', '/assets/');
               }
-            } 
+            }
           },
           {
             loader: 'image-webpack-loader'
@@ -43,8 +45,27 @@ module.exports = {
       jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: 'static', 
+      analyzerMode: 'disable'
+    }),
+    new WebpackPwaManifest({
+      name: 'Food Event',
+      short_name: 'Foodies',
+      description: 'An app that allows you to view upcoming food events.',
+      start_url: '../index.html',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve('assets/img/icons/icon-512x512.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons')
+        }
+      ]
     })
   ],
   mode: 'development'
 };
+
+module.exports = config;
